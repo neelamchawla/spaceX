a401c448390d8853a272fad0ea954c42eed52b53
neelamchawla <neelamchawla55@gmail.com>
2020-07-31 14:19:40 +0530

    Updates

---
+++ precache-manifest.e45cc4c1b7898a14418e1e24c74cd4c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd9036e31ac803710c259c8e591502b",
+    "url": "/spaceX/index.html"
+  },
+  {
+    "revision": "91b15d7f76ef338ca808",
+    "url": "/spaceX/static/css/main.366daf9f.chunk.css"
+  },
+  {
+    "revision": "4b4dc8123a382f51750f",
+    "url": "/spaceX/static/js/2.1c4f019d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spaceX/static/js/2.1c4f019d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b15d7f76ef338ca808",
+    "url": "/spaceX/static/js/main.93de6717.chunk.js"
+  },
+  {
+    "revision": "674d2da5a744fff73ed5",
+    "url": "/spaceX/static/js/runtime-main.bf6e654a.js"
+  },
+  {
+    "revision": "2f1befb2cdedbdf432ae1c3159ea759a",
+    "url": "/spaceX/static/media/astronaut.2f1befb2.png"
+  },
+  {
+    "revision": "07bbe94019dd2091b415c92a0d55b4e7",
+    "url": "/spaceX/static/media/bus.07bbe940.png"
+  },
+  {
+    "revision": "0c04533ae5500874ef38a55904251980",
+    "url": "/spaceX/static/media/engines.0c04533a.png"
+  },
+  {
+    "revision": "01fea637ed04ce1829e7a90393d1b8e3",
+    "url": "/spaceX/static/media/rocket1.01fea637.png"
+  },
+  {
+    "revision": "48d285a2dc121c9b08621f9f8a18af50",
+    "url": "/spaceX/static/media/rocket2.48d285a2.png"
+  },
+  {
+    "revision": "a1704019e4c8c2104daca0362800be25",
+    "url": "/spaceX/static/media/rocket3.a1704019.png"
+  },
+  {
+    "revision": "0960bb8096e20c433be184815f2462ec",
+    "url": "/spaceX/static/media/smoke.0960bb80.png"
+  },
+  {
+    "revision": "4a7f1dd2a5cafa5409c3aba867611360",
+    "url": "/spaceX/static/media/sunrise.4a7f1dd2.jpg"
+  }
+]);
